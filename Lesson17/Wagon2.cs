386582c7f30401fb7aa7c372e9c4f788a8eeccf5
ElinaVallianos <115302861+ElinaVallianos@users.noreply.github.com>
2023-02-11 19:09:33 +0100

    Lesson17

--- Lesson17/Wagon2.cs
+++ Lesson17/Wagon2.cs
@@ -1,11 +1,51 @@
 ﻿using System;
-namespace Lesson17
+using Lesson17;
+namespace Lesson17a
 {
-	public class Wagon2
-	{
-		public Wagon2()
-		{
-		}
-	}
+    public class Wagon2
+    {
+        private Lesson17.WagonType type;
+        private Wagon2? next;
+        private Wagon2? prev;
+
+        public WagonType Type { get => type; }
+        public Wagon2? Prev
+        {
+            get => prev;
+            set
+            {
+                prev = value;
+                if (value != null && value.Next != this)
+                {
+                    value.Next = this;
+                }
+
+                if (value != null)
+                {
+                    value.Next = this;
+                }
+                value.Next = this;
+            }
+        }
+        public Wagon2? Next
+        {
+            get => next;
+            set
+            {
+                next = value;
+                if(value != null)
+                {
+                    value.Prev = this;
+                }
+                value.Prev = this;
+
+            }
+        }
+
+        public Wagon2(WagonType type)
+        {
+            this.type = type;
+        }
+    }
 }
 
